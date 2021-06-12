6095a8b326820bf8fa825eff620d4d9ea2f0d53e
Nabaasa Richard <nabaasarichard@gmail.com>
2021-06-12 04:53:39 +0300

    Fix login with empty email and empty password

--- api/controller/user.go
+++ api/controller/user.go
@@ -43,10 +43,11 @@ func (cont *Controller) Login(c echo.Context) (err error) {
   if err := c.Bind(login); err != nil {
     return c.JSON(http.StatusInternalServerError, err)
   }
+
   cont.DB.Where("email = ?", login.Email).First(&user)
   matched := utils.CompareHashAndPassword(user.Password, login.Password)
 
-  if matched {
+  if matched && user.Name != "" {
     if t, err := utils.CreateJwtToken(user); err != nil {
       return c.JSON(http.StatusInternalServerError, err)
     } else {
